ccedf73ea6ab2d4fc854fbe72111e2e551f68952
unknown <dixitgarima24@gmail.com>
2024-09-05 15:43:53 +0530

    space

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -12,7 +12,7 @@ function Counter() {
   return (
     <div>
       <h1>Counter App</h1>
-      <p>Count:{count}</p>
+      <p>Count: {count}</p>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
     </div>
